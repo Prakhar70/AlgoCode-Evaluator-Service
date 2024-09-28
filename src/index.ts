68f68ec181c37f3e9f91e54109b3d5999927eb8f
Prakhar70 <42569845+Prakhar70@users.noreply.github.com>
2024-09-28 23:00:05 +0530

    Compilation Error in project

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express, {Express} from 'express';
 import serverConfig from './config/serverConfig';
+import apiRouter from './routes/v1';
 
 const app:Express = express();
+
+app.use('/api', apiRouter);
 
 app.listen(serverConfig.PORT, ()=>{
     console.log(`server started at *:${serverConfig.PORT}`);

01aba3b30503d257873e2a6a89ea4fe48328d8d1
Prakhar70 <42569845+Prakhar70@users.noreply.github.com>
2024-09-29 23:15:46 +0530

    "Bull mq based redis queue infra up and running"

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
-import express, {Express} from 'express';
-import serverConfig from './config/serverConfig';
-import apiRouter from './routes';
+import express, { Express } from "express";
+import serverConfig from "./config/serverConfig";
+import SampleQueueProducer from "./producers/sampleQueueProducer";
+import apiRouter from "./routes";
+import SampleWorker from "./workers/SampleWorker";
 
-const app:Express = express();
+const app: Express = express();
 
-app.use('/api', apiRouter);
+app.use("/api", apiRouter);
 
-app.listen(serverConfig.PORT, ()=>{
-    console.log(`server started at *:${serverConfig.PORT}`);
+app.listen(serverConfig.PORT, () => {
+  console.log(`server started at *:${serverConfig.PORT}`);
+
+  SampleWorker('SampleQueue');
+  SampleQueueProducer("SampleJob", {
+    name: "Prakhar",
+    company: "Microsoft",
+    position: "SDE2",
+    location: "Remote|BLR|Noida",
+  });
 });
--- src/jobs/SampleJob.ts
+++ src/jobs/SampleJob.ts
@@ -9,8 +9,13 @@ export default class SampleJob implements IJob {
     this.payload = payload;
     this.name = this.constructor.name;
   }
-  handle = () => {
+  handle = (job?:Job) => {
     console.log("Handler of job called");
+    console.log(this.payload);
+    
+    if(job){
+        console.log(job.name, job.id, job.data);
+    }
   };
   failed = (job?: Job): void => {
     console.log("job failed");
